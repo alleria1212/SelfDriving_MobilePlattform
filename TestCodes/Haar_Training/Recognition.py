d84e8c1e07483478545cf4fb673ff74efbba6a2f
Chun <chun-kan.chow@rwth-aachen.de>
2016-01-06 00:51:56 +0800

    object_detection

---
+++ TestCodes/Haar_Training/Recognition.py
@@ -0,0 +1,45 @@
+import cv2
+import sys
+
+
+def Recognition():
+    StopCascade = cv2.CascadeClassifier('stop_sign.xml')
+    LightCascade = cv2.CascadeClassifier('traffic_light.xml')
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        stopps = StopCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        lights = LightCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        for (x, y, w, h) in stopps:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        for (x, y, w, h) in lights:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+def main():
+    Recognition()
+    
+if __name__ == "__main__":
+    main()
